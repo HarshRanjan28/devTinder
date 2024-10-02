8ed3506954339763bae3b7287246df22f91499d3
Harsh Ranjan <harshranjan2897@gmail.com>
2024-10-02 16:49:06 +0530

    exploring a get post and delete

--- src/app.js
+++ src/app.js
@@ -2,9 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
-    res.send("Hello from the server")
+
+app.use("/users", (req, res) => {
+    res.send("This route will match with all HTTP methods")
 })
+
+app.get("/users", (req, res) => {
+    res.send({firstName: "Harsh", lastName: "Ranjan"});
+})
+
+app.post("/users", (req, res) => {
+    res.send("Data Successfully saved to Database");
+})
+
+app.delete("/users", (req, res) => {
+    res.send("Data Deleted Successfully");
+})
+
 
 app.listen(3000, () => {
     console.log('Server Started Successfully');
