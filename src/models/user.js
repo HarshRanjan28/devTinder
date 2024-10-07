709a752be0f0e43f6bdbb91ed58457a0942c0670
Harsh Ranjan <harshranjan2897@gmail.com>
2024-10-07 20:21:39 +0530

    schema validation

--- src/models/user.js
+++ src/models/user.js
@@ -3,23 +3,34 @@ const {Schema} = mongoose;
 
 const userSchema = new Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true
     },
     lastName: {
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        required: true,
+        unique: true
     },
     password: {
-        type: String
+        type: String,
+        required: true
     },
     age: {
-        type: String
+        type: String,
+        validate(value) {
+            if (!["male", "female", "others"].includes(value)) {
+                throw new Error("Not a valid Gender")
+            }
+        }
     },
     gender: {
         type: String
     }
+}, {
+    timestamps: true
 });
 
 const User = mongoose.model("User", userSchema);
